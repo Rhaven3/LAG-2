c7f351faea41f547e761da94ffeb5bc29d75174b
Théo Fontaine <105242602+Rhaven3@users.noreply.github.com>
2026-02-19 22:31:49 +0000

    some adjustment

--- src/commands/initiative.ts
+++ src/commands/initiative.ts
@@ -1,21 +1,25 @@
+import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
 
+@ApplyOptions<Command.Options>({
+  description: 'Créer un trackeur d\'initative'
+})
 export class InitiativeCommand extends Command {
   //Ajout de la commande dans le registre de commandes de l'application
   public override registerApplicationCommands(registry: Command.Registry) {
     registry.registerChatInputCommand((builder) => builder
-      .setName('initiative')
-      .setDescription('Créer un trackeur d\'initative')
+      .setName(this.name)
+      .setDescription(this.description)
       .addStringOption(option =>
-        option.setName('idsheets')
-          .setDescription('ajouté l\'id des fiches que vous souhaité utilisé, séparé d\'une virgule '),
+        option.setName('idSheet')
+          .setDescription('Ajouté l\'id des fiches que vous souhaité utilisé, séparé d\'une virgule'),
       )
     );
   }
 
 
   // Programme Executé lors de l'appel de la commande
-  // public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-  //   console.log("test");
-  // }
+  public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    console.log("test");
+  }
 }
